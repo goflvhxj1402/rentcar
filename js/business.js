a7fa9fd1ea29cb02bc00baecf283156c69a65a6b
goflvhxj1402 <61794054+goflvhxj1402@users.noreply.github.com>
2024-04-30 14:01:06 +0900

    Add files via upload

--- js/business.js
+++ js/business.js
@@ -1,21 +1,20 @@
 const businessCardWrap = document.querySelector("#businessCard ul");
 const cardList = document.querySelectorAll("#businessCard ul li");
-
-businessCardWrap.addEventListener("mouseenter", () => {
-    if(screenWidth < 780) return;
-    FlipCard();
-});
-businessCardWrap.addEventListener("mouseleave", () => {
-    if(screenWidth < 780) return;
-    FlipCard();
-});
-function FlipCard(){
-    if(isFlip){
-        cardList[0].style.transform = "rotateY(0deg)";
-        cardList[1].style.transform = "rotateY(180deg)";
-    }
-    else{
+let isMobile = false;
+window.addEventListener("resize", ()=>{
+    if (screenWidth < 780 && !isMobile){
+        isMobile = true;
         cardList[0].style.transform = "rotateY(180deg)";
         cardList[1].style.transform = "rotateY(360deg)";
     }
-}
+})
+businessCardWrap.addEventListener("mouseenter", () => {
+    if (screenWidth < 780) return;
+    cardList[0].style.transform = "rotateY(180deg)";
+    cardList[1].style.transform = "rotateY(360deg)";
+});
+businessCardWrap.addEventListener("mouseleave", () => {
+    if (screenWidth < 780) return;
+    cardList[0].style.transform = "rotateY(0deg)";
+    cardList[1].style.transform = "rotateY(180deg)";
+});
