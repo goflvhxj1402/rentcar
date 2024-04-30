e7974957117e0713c8dd828c7381acbfc9da37ed
goflvhxj1402 <61794054+goflvhxj1402@users.noreply.github.com>
2024-04-30 11:35:26 +0900

    Add files via upload

--- scrollManager.js
+++ scrollManager.js
@@ -5,10 +5,12 @@ let maxScrollIndex = arrSection.length;
 let curScrollIndex = 0;
 let isMotoDone = false;
 let firstTouchPos;
+let isLoaded = false;
 //리로드이벤트
 window.addEventListener("DOMContentLoaded", function (event) {
     setTimeout(() => {
         window.scroll(0, 0);
+        isLoaded = true;
     }, this.performance.now());
 })
 //리사이즈이벤트
@@ -17,21 +19,24 @@ window.addEventListener("resize", function (event) {
 });
 //스크롤이벤트
 window.addEventListener("wheel", function (event) {
+    if (!isLoaded) return;
     ScrollSection((event.deltaY > 0) ? 1 : -1);
 });
 //터치이벤트
-window.addEventListener("touchstart", function(event){
+window.addEventListener("touchstart", function (event) {
+    if (!isLoaded) return;
     firstTouchPos = event.touches[0].pageY;
 });
-window.addEventListener("touchend", function(event){
+window.addEventListener("touchend", function (event) {
+    if (!isLoaded) return;
     let gap = firstTouchPos - event.changedTouches[0].pageY;
-    if(gap > -49 && gap < 49){
+    if (gap > -49 && gap < 49) {
         return;
-    } 
-    else if(gap > 50){
+    }
+    else if (gap > 50) {
         gap = 1;
     }
-    else{
+    else {
         gap = -1;
     }
     ScrollSection(gap);
@@ -44,6 +49,25 @@ function ScrollSection(dir) {
     }
     if (isScrolling) return;
     isScrolling = true;
+    switch (curScrollIndex) {
+        case 0:
+            if (!isCarSelectDone) {
+                ShowCarSelect();
+            }
+            break;
+        case 1:
+            if (!isSolatiDone) {
+                ShowSolati();
+            }
+            break;
+        case 2:
+        case 3:
+            if (!isMotoDone) {
+                ScrollMoto();
+                if(curScrollIndex != 2)  return;
+            }
+            break;
+    }
     if (curScrollIndex == 3 && !isMotoDone) {
         ScrollMoto();
         return;
